ca3329d74a77de10d86d01ea2c315dafb0b28630
Supriya Das <bantibuto2@gmail.com>
2026-06-08 19:16:23 +0530

    chore: initialize package.json for basic deployment with express dependency

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express();
+const port = 8000;
+app.get("/health", (req, res) => {
+  res.send({ message: "Health is good" });
+});
+
+app.get("/", (req, res) => {
+  res.send({
+    message: "Yoh Man",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`App is running on port: ${port}`);
+});
